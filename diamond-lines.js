653ef8f8e9981c72cdce1086e219ac7eb617499d
bettyhahaha <55331305+bettyhahaha@users.noreply.github.com>
2019-10-14 00:20:46 -0700

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -5,3 +5,17 @@
 (10, 210) -> (390, 210)
 (400, 0) -> (0, 400)
 
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  for (var i = 10; i <= 390; i = i + 10) {
+    var startX = 200;
+    var startY = i;
+    var endX = 200;
+    var endY = i;
+    line(startX-i, startY, endX+i, endY);
+  }
+}
